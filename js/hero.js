f37fec94f5b3fe5b508de16fb7e78a2e985a083f
pjazdzewski1990 <pjazdzewski1990@gmail.com>
2015-02-22 20:19:18 +0100

    add tabs

--- js/hero.js
+++ js/hero.js
@@ -1,6 +1,6 @@
 (function(window, $){
   console.log("Start!");
-  var skills = [
+  var skillsBackend = [
     {name: "Scala", logo: "http://www.gravity.com/wp-content/uploads/2013/07/scala.gif"},
     {name: "Slick", logo: "http://slick.typesafe.com/resources/images/slick-logo.png"},
     {name: "Play", logo: "https://www.playframework.com/assets/images/logos/play_full_color.svg"},
@@ -11,18 +11,25 @@
     {name: "Spray", logo: "http://spray.io/img/logo-large.png"},
     {name: "Spark", logo: "https://spark.apache.org/images/spark-logo.png"},
     {name: "Finagle", logo: "https://pbs.twimg.com/media/BXhBtGoCYAAFwwJ.png"},
-    {name: "Scaloid", logo: "https://pbs.twimg.com/profile_images/3766836527/6d7533083289e80aba474c66650e0451_400x400.png"},
     {name: "Sbt", logo: "http://eed3si9n.github.io/sbt-logo/sbt-logo-orange-200x120.png"},
     {name: "Scalding", logo: "https://camo.githubusercontent.com/dd137c31e4abf89d794a89f8b8e379130fb06d6f/68747470733a2f2f7261772e6769746875622e636f6d2f747769747465722f7363616c64696e672f646576656c6f702f6c6f676f2f7363616c64696e672e706e67"}
   ];
-  
-  var skillList = $("#skills-list");
-  skills.forEach(function(skill){
-    skillList.append('<li> <img data-name="' + skill.name + '" class="skill-img grey" src="' + skill.logo + '"></img> </li>');
+  var skillListBackend = $("#skills-list-backend");
+  skillsBackend.forEach(function(skill){
+    skillListBackend.append('<li> <img data-name="' + skill.name + '" class="skill-img grey" src="' + skill.logo + '"></img> </li>');
   });
   
   $(".skill-img").on("click", function(evt){
     console.log("Clicked " + $(this).attr("data-name"), evt);
     $(this).toggleClass("grey");
+  });
+  
+  $(".tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
   });
 })(window, $);
